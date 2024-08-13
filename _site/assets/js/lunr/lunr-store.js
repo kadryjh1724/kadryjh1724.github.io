99193d63007d624854be6885765bc1db2fa80e98
commuteneko <paradichlorobenzene1724@gmail.com>
2024-08-13 17:00:07 +0900

    changed base url from blog to default

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,13 +1,19 @@
 var store = [{
         "title": "Introduction to the Diffusion Models [1]",
-        "excerpt":"Mathjax test   \\[\\int_{0}^{T}\\lambda(t)\\mathbb{E}_{0}\\mathbb{E}_{X_{t}|X_{0}}\\left[s_{\\theta}(X_{t},t)-\\nabla_{X_{t}}\\log p_{t|0}(X_{t}|X_{0})^{2}\\right]\\]  Test 2   \\[ a(s) = \\frac{\\partial\\mathcal{L}}{\\partial z(s)} \\]  ","categories": ["DL"],
+        "excerpt":"Mathjax test   \\[\\int_{0}^{T}\\lambda(t)\\mathbb{E}_{0}\\mathbb{E}_{X_{t}|X_{0}}\\left[s_{\\theta}(X_{t},t)-\\nabla_{X_{t}}\\log p_{t|0}(X_{t}|X_{0})^{2}\\right]\\]  Test 2   \\[ a(s) = \\frac{\\partial\\mathcal{L}}{\\partial z(s)} \\]   To be deleted later.  ","categories": ["DL"],
         "tags": ["DL","diffusion"],
-        "url": "/blog/dl/example/",
+        "url": "/dl/example/",
         "teaser": null
       },{
         "title": "Introduction to CUDA [1]",
         "excerpt":"Introduction to CUDA C/C++ CUDA C/C++를 공부하고 간단한 계산에 적용해 보는 과정을 포스팅으로 남길 예정이다. CUDA C/C++를 공부하는 방법에는 여러 가지가 있다. 시중에 CUDA C/C++를 다루는 책도 여러 권 나와 있다 (한국어 화자인 나 역시 처음에는 이런 책들을 참고해 공부하고 있다). 하지만 무엇보다 CUDA C/C++를 공부할 때의 golden standard는 NVIDIA가...","categories": ["Programming"],
         "tags": ["CUDA","C","C++"],
-        "url": "/blog/programming/introduction-to-cuda-01/",
+        "url": "/programming/introduction-to-cuda-01/",
+        "teaser": null
+      },{
+        "title": "Introduction to CUDA [2]",
+        "excerpt":"Host-device interaction 이전 포스팅에서 다루었듯이, host와 device는 물리적으로 분리되어 있다. 따라서, host와 device는 독립적인 메모리를 가지고 있으며, memory allocation 역시 host와 device에서 각각 진행해야 한다. 뿐만 아니라, host와 device 사이에서 데이터를 교환하는 것 역시 자명하지 않다. 이번 포스팅에서는 host와 device에서의 memory allocation과 데이터 교환을 위해 제공되는 CUDA C/C++ 함수들을 다룰...","categories": ["Programming"],
+        "tags": ["CUDA","C","C++"],
+        "url": "/programming/introduction-to-cuda-02/",
         "teaser": null
       }]
