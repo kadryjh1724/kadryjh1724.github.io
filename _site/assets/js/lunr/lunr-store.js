eecc9d2dcf6a5285c655e695ea247122d753b2a0
commuteneko <paradichlorobenzene1724@gmail.com>
2025-06-25 18:16:28 +0900

    fixed curly braces issue

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -6,7 +6,7 @@ var store = [{
         "teaser": null
       },{
         "title": "[MTP] 1-1. Algebra and $\\sigma$-algebra",
-        "excerpt":"Chapter 1. Measure Spaces 1-1. Algebra and $\\sigma$-algebra Let $S$ be a set without any given topology and $\\mathcal{P}(S) = { A \\mid A \\subseteq S }$ be a power set of $S$. Definition 1.1.1 The collection of sets $\\Sigma_{0} \\subseteq \\mathcal{P}(S)$ is called an algebra on $S$ if (i)...","categories": ["Measure Theory"],
+        "excerpt":"Chapter 1. Measure Spaces 1-1. Algebra and $\\sigma$-algebra Algebra Let $S$ be a set without any given topology and $\\mathcal{P}(S) = \\lbrace A \\mid A \\subseteq S \\rbrace$ be a power set of $S$. Definition 1.1.1 The collection of sets $\\Sigma_{0} \\subseteq \\mathcal{P}(S)$ is called an algebra on $S$ if...","categories": ["Measure Theory"],
         "tags": ["mathematics","measure theory"],
         "url": "/measure%20theory/MTP01/",
         "teaser": null
