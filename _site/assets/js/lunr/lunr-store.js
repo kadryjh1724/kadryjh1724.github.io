85f89ef69cba0501eb45cb6a64c8ba7378d27dff
commuteneko <paradichlorobenzene1724@gmail.com>
2025-06-28 16:07:02 +0900

    Added one post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,13 +1,13 @@
 var store = [{
-        "title": "Kac ring model",
-        "excerpt":"Blah Blah  ","categories": ["Nonequilibrium Statistical Mechanics"],
-        "tags": ["nonequilibrium","Python"],
-        "url": "/nonequilibrium%20statistical%20mechanics/Kac-ring-model/",
-        "teaser": null
-      },{
         "title": "[MTP] 1-1. Algebra and $\\sigma$-algebra",
         "excerpt":"Chapter 1. Measure Spaces 1-1. Algebra and $\\sigma$-algebra Algebra Let $S$ be a set without any given topology and $\\mathcal{P}(S) = \\lbrace A \\mid A \\subseteq S \\rbrace$ be a power set of $S$. Definition 1.1.1 The collection of sets $\\Sigma_{0} \\subseteq \\mathcal{P}(S)$ is called an algebra on $S$ if...","categories": ["Measure Theory"],
         "tags": ["mathematics","measure theory"],
         "url": "/measure%20theory/MTP01/",
+        "teaser": null
+      },{
+        "title": "[MTP] 1-2. Measure",
+        "excerpt":"Chapter 1. Measure Spaces 1-2. Measure Measure and measure spaces In the definitions of both $\\sigma$-algebras and measures, countable unions and countable additivity are important. Definition 1.2.1 Let $\\Sigma_{0}$ be an algebra on $S$. Let $\\mu_{0} : \\Sigma_{0} \\rightarrow [0, \\infty]$ be a set function. (a) $\\mu_{0}$ is additive if...","categories": ["Measure Theory"],
+        "tags": ["mathematics","measure theory"],
+        "url": "/measure%20theory/MTP02/",
         "teaser": null
       }]
